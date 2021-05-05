5fc96419243d8e9741be052ae8bc40ccd42849b3
Ismael Dominguez <ismaeldominguezcampos98@gmail.com>
2021-05-05 17:12:41 +0200

    Añadiendo vistas y controladores

---
+++ Controllers/ReservaController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PadelWebXerez.Controllers
+{
+    public class ReservaController : Controller
+    {
+        public IActionResult Reserva()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/SobreNosotrosController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PadelWebXerez.Controllers
+{
+    public class SobreNosotrosController : Controller
+    {
+        public IActionResult SobreNosotros()
+        {
+            return View();
+        }
+    }
+}
